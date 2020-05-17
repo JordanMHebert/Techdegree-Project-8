950bb4799a81e9b4a0692959edfb14a7d0257ad8
JordanMHebert <55060700+JordanMHebert@users.noreply.github.com>
2020-05-17 11:32:53 -0400

    update to HTML, CSS, and JavaScript

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,33 +1,6 @@
-const select = document.getElementById("gallery");
-const card = document.querySelector(".directory");
-const Info = document.querySelector("employee1");
-
-// ------------------------------------------
-//  FETCH FUNCTIONS
-// ------------------------------------------
-
-fetch("https://dog.ceo/api/breeds/list")
-  .then(response => response.json())
-  .then(data => generateOptions(data.message));
-
 fetch("https://randomuser.me/")
   .then(response => response.json())
   .then(data => generateImage(data.message));
-
-// ------------------------------------------
-//  HELPER FUNCTIONS
-// ------------------------------------------
-
-function generateOptions(data) {
-  const options = data
-    .map(
-      item => `
-      <option value='${item}'>${item}</option>
-    `
-    )
-    .join("");
-  select.innerHTML = options;
-}
 
 function generateImage(data) {
   const html = `
@@ -36,7 +9,3 @@ function generateImage(data) {
     `;
   card.innerHTML = html;
 }
-
-// ------------------------------------------
-//  EVENT LISTENERS
-// ------------------------------------------
